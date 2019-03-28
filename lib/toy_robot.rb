50ee17de6b0aae2eae3ddb19cc53284c9ef41275
ahmadelmorshedy <ah.morshedy@gmail.com>
2019-03-28 23:20:25 +0200

    [test] : Adding test cases (iteration1: Build basic implementation of ToyRobot)

---
+++ lib/toy_robot.rb
@@ -0,0 +1,10 @@
+class ToyRobot
+  def initialize(table_top = TableTop.new)
+    @table_top = table_top
+  end
+
+  def process_commands(commands_arr)
+    # process given commands array
+    # should outputs REPORT command to stdout
+  end
+end
